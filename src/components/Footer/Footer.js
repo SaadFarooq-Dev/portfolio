afdedd266e190f9316480b6595a340278056cfea
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-21 18:23:44 +0500

    Completed footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+
+import Layout from '../Layout'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg '>
+      <Layout className='py-8 flex items-center justify-between '>
+        <span >{new Date().getFullYear()} &copy; All Rights Reserved. </span>
+        <div className='flex items-center'>
+          <Link target='_blank' href='https://github.com/SaadFarooq-Dev' className='underline underline-offset-4'>SaadFarooq-Dev</Link>
+        </div>
+        <Link target='_blank' href='https://github.com/SaadFarooq-Dev'>Say Hello</Link>
+      </Layout>
+    </footer>
   )
 }
 
