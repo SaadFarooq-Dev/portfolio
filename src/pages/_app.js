568d3f02cccb8ae48be52aa3fb9f76be867b526a
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-22 22:19:45 +0500

    About page header and number animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,7 @@
 import { Montserrat } from 'next/font/google'
-
-import NavBar from '@/components/NavBar'
 import Head from 'next/head'
 
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import Footer from '@/components/Footer'
 
