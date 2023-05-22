7ce9d033a1866633a90b851283d3e32beccbc096
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-22 22:21:59 +0500

    Created Animated numbers component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,32 +1,10 @@
+import React from 'react'
 import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
-import React, { useEffect, useRef } from 'react'
 import profileImage from '/public/images/profile/developer-pic-2.jpg'
-import { useInView, useMotionValue, useSpring } from 'framer-motion'
-
-const AnimatedNumbers = ({ value }) => {
-  const ref = useRef(null)
-  const motionValue = useMotionValue(0)
-  const springValue = useSpring(motionValue, { duration: 3000 })
-  const isInView = useInView(ref, { once: true })
-  useEffect(() => {
-    if (isInView) {
-      motionValue.set(value)
-    }
-  }, [isInView, value, motionValue]);
-
-  useEffect(() => {
-    springValue.on('change', (latest) => {
-      if (ref.current && latest.toFixed(0) <= value) {
-        ref.current.textContent = latest.toFixed(0)
-      }
-    })
-  }, [springValue, value]);
-
-  return <span ref={ref}></span>
-}
+import AnimatedNumbers from '@/components/AnimatedNumbers'
 
 const about = () => {
   return (
