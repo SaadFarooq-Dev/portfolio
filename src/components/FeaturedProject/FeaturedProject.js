3b73a154cc542a322bfdf96edf2a71b2cc65966a
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-23 00:35:57 +0500

    Completed FeatureProject Component

--- src/components/FeaturedProject/FeaturedProject.js
+++ src/components/FeaturedProject/FeaturedProject.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GithubIcon } from '../Icons'
 
-const FeaturedProject = () => {
+const FeaturedProject = ({ type, title, summary, image, link, github }) => {
   return (
-    <div>FeaturedProject</div>
+    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 '>
+      <Link href={link} target='_blank'className='w-1/2 cursor-pointer overflow-hidden rounded-lg' >
+        <Image src={image} alt={title} className='w-full h-auto'/>
+      </Link>
+        <div className='w-1/2 flex flex-col items-start justify-between pl-6 '>
+          <span className='text-primary font-medium text-xl'>
+            {type}
+          </span>
+          <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
+            <h2 className='my-2 w-full text-left text-4xl font-bold' >{title}</h2>
+          </Link>
+          <p className='my-2 font-medium text-dark '>{summary}</p>
+          <div className='mt-2 flex items-center ' >
+            <Link href={github} target='_blank' className='w-10'> <GithubIcon /> </Link>
+            <Link href={link} target='_blank' className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold '> Visit Project </Link>
+          </div>
+        </div>
+    </article>
   )
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Head from 'next/head'
 import Layout from '@/components/Layout'
 import AnimatedText from '@/components/AnimatedText'
+import FeaturedProject from '@/components/FeaturedProject'
+import projectImage1 from  '/public/images/projects/crypto-screener-cover-image.jpg'
 
 const projects = () => {
   return (
@@ -12,10 +14,18 @@ const projects = () => {
       </Head>
       <main className='w-full mb-16 flex flex-col items-center justify-center'>
         <Layout className='pt-16'>
-          <AnimatedText text={'Imagination Trumps Knowledge!'} />
+          <AnimatedText text={'Imagination Trumps Knowledge!'} className='mb-16 ' />
           <div className='grid grid-cols-12 gap-24'>
             <div className='col-span-12'>
-              Featured Project
+              <FeaturedProject
+               title={'Crypto Screener Application'}
+              summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'}
+                link={'/'}
+                type={'Featured Project'}
+                github={'www.github.com'}
+                image={projectImage1}
+              />
+
             </div>
             <div className='col-span-6'>
               Project-1
