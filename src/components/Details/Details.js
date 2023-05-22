d2d60abb142a4b176e1edf6fafc9b6e6f253b051
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-22 23:47:56 +0500

    Created ListIcon and its Animation

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,22 @@
+import { useRef } from "react"
+import ListIcon from "../ListIcon"
+import { motion } from "framer-motion"
+
 const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null)
+
   return (
-    <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between '>
-      <div>
+    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between '>
+      <ListIcon reference={ref}/>
+      <motion.div
+      initial={{y:50}}
+      whileInView={{y:0}}
+      transition={{duration: 0.5, type: "spring"}}
+      >
         <h3 className='capitalize font-bold text-2xl' >{position} &nbsp; <a target='_blank' className='text-primary capitalize' href={companyLink}>@{company} </a> </h3>
         <span className='capitalize font-medium text-dark/75'>{time} | {address}</span>
         <p className='font-medium w-full' >{work}</p>
-      </div>
+      </motion.div>
     </li>
   )
 }
--- src/components/ListIcon/ListIcon.js
+++ src/components/ListIcon/ListIcon.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion, useScroll } from 'framer-motion'
 
-const ListIcon = () => {
+const ListIcon = ({reference}) => {
+
+  const { scrollYProgress } = useScroll(
+    {
+      target: reference,
+      offset: ['center end', 'center center']
+    }
+  )
   return (
-    <div>ListIcon</div>
+    <figure className='absolute left-0 stroke-dark '>
+      <svg className='-rotate-90' width={'75'} height={'75'} viewBox='0 0 100 100'>
+        <motion.circle cx={'75'} cy={'50'} r={'20'} className='stroke-primary stroke-1 fill-none' />
+        <motion.circle cx={'75'} cy={'50'} r={'20'} className='stroke-[5px] fill-light'
+        style={{
+          pathLength:scrollYProgress
+        }}
+        />
+        <motion.circle cx={'75'} cy={'50'} r={'10'} className=' animate-pulse stroke-1 fill-primary'/>
+      </svg>
+    </figure>
   )
 }
 
